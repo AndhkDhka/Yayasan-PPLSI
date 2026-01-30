3438de6a3b9cd194402279c0c5c7f6143d873582
AndhkDhka <dhika.cr.05@gmail.com>
2026-01-30 21:16:21 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 </head>
 
-<body class="m-0 p-0 font-sans text-gray-800">
+<body class="bg-red-500 text-white text-3xl">
 
     {{-- NAVBAR --}}
     <nav class="fixed top-6 left-0 right-0 z-[9999] flex justify-center">
