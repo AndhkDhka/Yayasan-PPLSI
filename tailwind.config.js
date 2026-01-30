820b5ebddb87d37090ee98b44cbf87d15aea5548
Dhika <dhika.cr.05@gmail.com>
2026-01-30 16:34:28 +0700

    Fix Tailwind root config and production CSS build

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,13 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 import forms from '@tailwindcss/forms'
 
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: [
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.js',
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
   ],
-
   theme: {
     extend: {
       fontFamily: {
